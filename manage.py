8a76fa0096a2e8cf22d015081a4a4a098487db90
ronopedia <81117989+ronopedia@users.noreply.github.com>
2022-03-20 02:24:15 -0500

    Updated

--- manage.py
+++ manage.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-app = Flask(__name__)
-
 from app import app
 from app import db
 
