81a3d6a413a4114a30fde07c9818aa9843f6151e
ronopedia <81117989+ronopedia@users.noreply.github.com>
2022-03-19 19:36:03 -0500

    Updated

--- app/config.py
+++ app/config.py
@@ -6,6 +6,16 @@ load_dotenv()  # load environment variables from .env if it exists.
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
-    #SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://ocbhxhvivizadz:514420ea48a0a48b693f81d83012702ec02e1cc6a8882802707a38e58bcb49eb@ec2-54-211-176-156.compute-1.amazonaws.com:5432/d66pl8ogi5705n'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    UPLOAD_FOLDER = './uploads'
+    
+    class DevelopmentConfig(Config):
+    """Development Config that extends the Base Config Object"""
+    DEVELOPMENT = True
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Production Config that extends the Base Config Object"""
+    DEBUG = False
