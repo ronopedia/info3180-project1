d812a96cc712be5d253fd52737b468224206353d
ronopedia <81117989+ronopedia@users.noreply.github.com>
2022-03-19 19:48:59 -0500

    Updated

---
+++ app/models.py
@@ -0,0 +1,44 @@
+from . import db
+
+
+class PropertyInfo(db.Model):
+
+    id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    title = db.Column(db.String(255))
+    bathrooms = db.Column(db.String(2))
+    bedrooms = db.Column(db.String(2))
+    description = db.Column(db.String(255))
+    price = db.Column(db.String(80))
+    prop_type = db.Column(db.String(9))
+    location = db.Column(db.String(255))
+    photo = db.Column(db.String(255), index = True)
+
+    
+    def __init__(self, title, bathrooms, bedrooms, description, price, prop_type, location, photo):
+        self.title = title
+        self.bathrooms = bathrooms
+        self.bedrooms = bedrooms
+        self.description = description
+        self.price = price
+        self.prop_type = prop_type
+        self.location = location
+        self.photo = photo
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+         return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
+
+    def __repr__(self):
+      return '<User %r>' % (self.username)
+ 
