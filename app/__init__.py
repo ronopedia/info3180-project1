154e75bb7b152d03d5cae89d28b0f4d39cbffa7f
Ronaldo Salmon <ronopedia@gmail.com>
2022-03-19 22:45:10 -0500

    Updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from .config import Config
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
 app.config.from_object(Config)
 from app import views
--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = './uploads'
     
-    class DevelopmentConfig(Config):
+class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
     DEVELOPMENT = True
     DEBUG = True
