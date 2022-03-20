f1264cb8f23fd9d019c1311ed42fe906e5c7a637
Ronaldo Salmon <ronopedia@gmail.com>
2022-03-20 02:01:46 -0500

    Updated

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 #from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask._compat import text_type
 
 from app import app
 from app import db
